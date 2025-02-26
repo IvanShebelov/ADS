8f28a13afec27becaa6f590c78ddd1b864458346
IvanShebelov <131601125+IvanShebelov@users.noreply.github.com>
2025-02-26 20:50:04 +0300

    Added header file for TUnsortedTable

--- lib_itable/unsort_table.h
+++ lib_itable/unsort_table.h
@@ -3,6 +3,27 @@
 #ifndef LIB_ITable
 #define LIB_ITable
 
+#include "ITable.h"
+#include "TList.h"
+#include "TPair.h"
 
+template <typename TKey, typename TVal>
+class TUnsortedTable : public ITable<TKey, TVal> {
+private:
+    TList<TPair<TKey, TVal>> _data;  // Список для хранения пар ключ-значение
+
+public:
+    // Вставка элемента в таблицу
+    void insert(const TKey& key, const TVal& value) override;
+
+    // Удаление элемента из таблицы
+    void remove(const TKey& key) override;
+
+    // Поиск элемента по ключу
+    TVal* find(const TKey& key) override;
+
+    // Получение размера таблицы
+    size_t size() const override;
+};
 
 #endif  // LIB_ITable
